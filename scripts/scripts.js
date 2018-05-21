c668fb8b52529f5e952e7b4d8c5b7d4d919197f0
Bruno Bacelar Abe <messengerabe@hotmail.com>
2018-05-21 16:50:39 -0300

    Trabalho 2 de Web.
    Trabalho 2 de Web.
    SPA, com jQuery.

---
+++ scripts/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	$(".main").load("Menu/initialScreen.html"); 
+
+	/*Funções de direcionamento do menu, por enquanto, só há a mudança da tag <main> para a respectiva
+	nova tela.
+	Exemplo: Se foi clicado em "Home", todo o conteúdo entre a tag main será substituído.*/
+	$("#initialScreen").click(function(){
+		$(".main").load("Menu/initialScreen.html")
+	});	
+
+	$("#productsScreen").click(function(){
+		$(".main").load("Menu/productScreen.html")
+	});
+
+	$("#contactScreen").click(function(){
+		$(".main").load("Menu/contactScreen.html")
+	});
+
+	$("#aboutScreen").click(function(){
+		$(".body").load("Menu/aboutScreen.html")
+	});
+
+	$("#unityScreen").click(function(){
+		$(".main").load("Menu/unityScreen.html")
+	});
+
+	$("#structureScreen").click(function(){
+		$(".main").load("Menu/structureScreen.html")
+	});
+
+	$("#loginScreen").click(function(){
+		$(".main").load("Menu/loginScreen.html")
+	});
+
+		$("#buttonForm").click(function(){
+			alert("FUI APERTADO");
+		});			
+});
